3f45b7eb913908f1c1a994418ca9fd8a0722fa61
marton bognar <martonbognar@sch.bme.hu>
2019-09-09 11:16:56 +0200

    Clean up MainRunner

--- Tool/MainRunner.hs
+++ Tool/MainRunner.hs
@@ -1,19 +1,10 @@
-import           Data.Text.Prettyprint.Doc
-import           GeneralTerms
-import           MyParser
-import           System.Process
-import           Text.Parsec.Error
-import           Text.Parsec.String
-import           ToHaskellPrint
+{-# OPTIONS_GHC -Wall #-}
 
-toFileHaskell ::
-     Language
-  -> String
-  -> IO (Either (Text.Parsec.Error.ParseError) (Either String (Doc String)))
-toFileHaskell lang name =
-  case wellFormed lang of
-    Left failtxt2 -> return (Right (Left failtxt2))
-    Right _ -> return (Right (Right (toHaskellLanguageStart lang name)))
+import Data.Text.Prettyprint.Doc
+import GeneralTerms
+import MyParser
+import Text.Parsec.String
+import ToHaskellPrint
 
 main :: IO ()
 main = do
@@ -25,47 +16,11 @@ main = do
     Right lang -> do
       toWrite <- toFileHaskell lang "haskellOutput"
       case toWrite of
-        Right (Left err)    -> print err
-        Right (Right value) -> writeFile "HaskellOutput.hs" (show value)
+        Left err    -> print err
+        Right value -> writeFile "HaskellOutput.hs" (show value)
 
--- genLangNoFormat :: String -> IO ()
--- genLangNoFormat name = do
---   result <- parseFromFile pLanguage name
---   case result of
---     Left err -> print err
---     Right lang -> do
---       toWrite <- toFileHaskell lang "haskellOutput"
---       case toWrite of
---         Right (Left err) -> print err
---         Right (Right value) -> do
---           a <- writeFile "haskellOutput.hs" (show value)
---          -- b <- callCommand "hfmt -w"
---           return ()
-
--- genLang :: String -> IO ()
--- genLang name = do
---   result <- parseFromFile pLanguage name
---   case result of
---     Left err -> print err
---     Right lang -> do
---       toWrite <- toFileHaskell lang "haskellOutput"
---       case toWrite of
---         Right (Left err) -> print err
---         Right (Right value) -> do
---           a <- writeFile "haskellOutput.hs" (show value)
---           b <- callCommand "hfmt -w"
---           return ()
-
--- genLangName :: String -> String -> IO ()
--- genLangName nameinput nameoutput = do
---   result <- parseFromFile pLanguage nameinput
---   case result of
---     Left err -> print err
---     Right lang -> do
---       toWrite <- toFileHaskell lang nameoutput
---       case toWrite of
---         Right (Left err) -> print err
---         Right (Right value) -> do
---           a <- writeFile (nameoutput ++ ".hs") (show value)
---           b <- callCommand "hfmt -w"
---           return ()
+toFileHaskell :: Language -> String -> IO (Either String (Doc String))
+toFileHaskell lang name =
+  case wellFormed lang of
+    Left failtxt2 -> return (Left failtxt2)
+    Right _ -> return (Right (toHaskellLanguageStart lang name))
